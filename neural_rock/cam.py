f2c62a46a96cf727bccb2b098cd462347364ca82
lmoss <lukas.mosser@earthanalytics.no>
2021-04-05 18:48:24 +0200

    updated notebook

--- neural_rock/cam.py
+++ neural_rock/cam.py
@@ -58,13 +58,12 @@ class ModelOutputs():
 
 
 class GradCam:
-    def __init__(self, model, feature_module, target_layer_names, use_cuda):
+    def __init__(self, model, feature_module, target_layer_names, device='cpu'):
         self.model = model
         self.feature_module = feature_module
         self.model.eval()
-        self.cuda = use_cuda
-        if self.cuda:
-            self.model = model.cuda()
+        self.model = model.to(device)
+        self.device = device
 
         self.extractor = ModelOutputs(self.model, self.feature_module, target_layer_names)
 
@@ -72,8 +71,7 @@ class GradCam:
         return self.model(input_img)
 
     def __call__(self, input_img, target_category=None):
-        if self.cuda:
-            input_img = input_img.cuda()
+        input_img = input_img.to(self.device)
 
         features, output = self.extractor(input_img)
 
@@ -82,9 +80,7 @@ class GradCam:
 
         one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
         one_hot[0][target_category] = 1
-        one_hot = torch.from_numpy(one_hot).requires_grad_(True)
-        if self.cuda:
-            one_hot = one_hot.cuda()
+        one_hot = torch.from_numpy(one_hot, device=self.device).requires_grad_(True)
 
         one_hot = torch.sum(one_hot * output)
 
@@ -96,7 +92,6 @@ class GradCam:
 
         target = features[-1]
         target = target.cpu().data.numpy()[0, :]
-        print(grads_val.shape)
         weights = np.mean(grads_val, axis=(2, 3))[0, :]
         cam = np.zeros(target.shape[1:], dtype=np.float32)
 
